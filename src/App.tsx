3c375acad4987b5c300e0672efa714cd56d8b1af
Roberto Ramirez <robert.624@hotmail.com>
2023-07-18 15:02:41 -0500

    added country search form

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import './App.scss'
 import Layout from './Layout/Layout'
+import SearchCountry from './components/SearchCountry'
+import { useState } from 'react'
 
 function App() {
+
+  const [search, setSearch] = useState<string>("")
+
   return (
     <>
       <Layout >
-        <h1>
-          Hello, world!
-        </h1>
+        <SearchCountry searchTerm={search} setSearchTerm={setSearch} />
       </Layout>
     </>
   )
