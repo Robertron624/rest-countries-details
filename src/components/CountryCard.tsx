2879906085f5d7e678da4ec339bb0b2bc43495de
Roberto Ramirez <robert.624@hotmail.com>
2023-07-18 22:06:06 -0500

    stylized country cards

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -4,12 +4,12 @@ const CountryCard = (country: Country) => {
     const { name, population, region, capital, flags } = country;
 
     return (
-        <div className="country-card">
+        <div className="country-card text-left mx-auto">
             <div className="image-container">
                 <img src={flags.png} alt={flags.alt} />
             </div>
             <div className="bottom">
-                <h3 className="country-name">{name.official}</h3>
+                <h3 className="country-name">{name.common}</h3>
                 <div className="short-info">
                     <p>
                         <span>Population:</span> {population}
@@ -18,7 +18,7 @@ const CountryCard = (country: Country) => {
                         <span>Region:</span> {region}
                     </p>
                     <p>
-                        <span>Capital:</span> {capital}
+                        <span>Capital:</span> {capital ? capital : "N/A"}
                     </p>
                 </div>
             </div>
